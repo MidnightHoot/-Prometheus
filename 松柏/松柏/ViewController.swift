b20745b569ab7f055f9a2607bda7851b0fae250b
Departures <departures_egoist@icloud.com>
2017-07-16 16:43:57 +0800

    Start

---
+++ 松柏/松柏/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  松柏
+//
+//  Created by Yihan_G on 16/07/2017.
+//  Copyright © 2017 MidnightHoot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
